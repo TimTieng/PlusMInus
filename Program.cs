ea550503a7940c4ecd38fe5422bd677ecf89d2b5
TimTieng <timothy.tieng@gmail.com>
2021-03-12 05:50:59 -0500

    Pushing local files to GH

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PlusMinus
+{
+    class Program
+    {
+        static void PlusMinus(int[] array)
+        {
+            // declare variables for each type as counters
+            int posCount = 0;
+            int negCount = 0;
+            int zeroCount = 0;
+            // Determine if value is +, -, 0 => increment accordingly
+            for (int i =0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    posCount++;
+                }
+                else if (array[i] < 0)
+                {
+                    negCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+        double posRatio = (double)posCount/array.Length;
+        double negRatio = (double)negCount/array.Length;
+        double zeroRatio = (double)zeroCount/array.Length;
+        
+        Console.WriteLine($"Positive Number Ratio: {posRatio}");
+        Console.WriteLine($"Negative Number Ratio: {negRatio}");
+         Console.WriteLine($"Zero Ratio: {zeroRatio}");
+        }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int[] testArr = {1,-1,0,2,2,3,5};
+            PlusMinus(testArr);
+        }
+    }
+}
+
